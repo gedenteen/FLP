7a0fbe8959756729ce112636c59a4db47ee82336
gedenteen <razumow.dmitrij@yandex.ru>
2020-09-24 09:33:38 +0700

    Implement task3

--- lab1.lisp
+++ lab1.lisp
@@ -1,5 +1,7 @@
 (defun func1 (a) 
 	(car(cdr(cdr(car(cdr a))))))
 
-(print (caddr(cadr'(1 (2 (3 4) *) 5))))
-(print (cons '(a (b c)) nil))
+(print (caddr(cadr'(1 (2 (3 4) *) 5)))) ;task 1
+(print (cons '(a (b c)) nil)) ;task2
+(print (cons '(1) (cons '(2 3) nil))) ;task3a
+(print (list '(1) '(2 3))) ;task3b
